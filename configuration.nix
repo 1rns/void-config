d88d060c364f9169bf80e20b4320f98d9171982b
lxvrns <personal@laurensmorales.dev>
2023-05-01 20:17:51 +1200

    Backup b/c i almost lost everything

--- configuration.nix
+++ configuration.nix
@@ -26,10 +26,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  hardware.bumblebee = {
-    enable = true;
-    pmMethod = "none";
-  };
   
   boot.tmp.cleanOnBoot = true;
   boot.supportedFilesystems = [ "ntfs" ];
@@ -185,13 +181,16 @@
         packages = with rPackages; 
           [ quarto patchwork visdat simputation VIM  pandoc skimr ggplot2 dplyr tidyverse rmarkdown knitr ];
        };
-#      my-python-packages = ps: with ps; [
-#      
-#      ];
+      my-python-packages = ps: with ps; [
+      
+      ];
     in
    [
   #syspkgs
     wget
+    okular
+    gallery-dl
+    logiops
     google-chrome
     aria
     renameutils
@@ -205,7 +204,7 @@
     file
     texlive.combined.scheme-full
     falkon
-    #(python3.withPackages my-python-packages)
+    (python3.withPackages my-python-packages)
     pandoc
     rofi
     fd
@@ -274,12 +273,84 @@
   environment.shells = with pkgs; [ fish ];
   environment.binsh = "${pkgs.bash}/bin/bash";
 
+  # logiops
+  # enable using sudo logid
+  systemd.services.logiops = {
+    description = "An unofficial userspace driver for HID++ Logitech devices";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.logiops}/bin/logid";
+    };
+  };
+  environment.etc."logid.cfg".text = ''
+    devices: ({
+      name: "Wireless Mouse MX Master 3";
+      smartshift: {
+        on: true;
+        threshold: 12;
+      };
+      hiresscroll: {
+        hires: true;
+        target: false;
+      };
+      dpi: 800;
+      buttons: ({
+        cid: 0xc3;
+        action = {
+          type: "Gestures";
+          gestures: ({
+            direction: "Left";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F15"];
+            };
+          }, {
+            direction: "Right";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F16"];
+            };
+          }, {
+            direction: "Down";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F17"];
+            };
+          }, {
+            direction: "Up";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F18"];
+            };
+          }, {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_PLAYPAUSE"];
+            };
+          });
+        };
+      }, {
+        cid: 0x53;
+        action = {
+          type: "Keypress";
+          keys: ["KEY_SPACE"];
+        };
+      });
+    });
+  '';
   # fonts
   fonts.fonts = with pkgs; [
   noto-fonts
   noto-fonts-cjk
   noto-fonts-emoji
   hasklig
+  google-fonts
   hack-font
   powerline-fonts
   siji
